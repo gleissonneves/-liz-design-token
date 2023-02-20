f2ac6fca34c36fedcd352b6ca439145c3ff82239
Gleisson Neves <gleissonenves2017@gmail.com>
2023-02-20 03:18:48 -0300

    fix: expondo css mimify ate resolucao de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,34 @@ const path = require("path");
 // const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
+const buildPRD = false;
+
 module.exports = {
-  mode: 'production',
+  mode:  (buildPRD) ? 'production' : 'development',
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
+  module: {
+    rules: [
+      {
+        // For pure CSS - /\.css$/i,
+        // For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
+        // For Less - /\.((c|le)ss)$/i,
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              // Run `postcss-loader` on each CSS `@import` and CSS modules/ICSS imports, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+              // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
